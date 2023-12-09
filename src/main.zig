0418eeb52fca1957980af256b22a8e5b73c0794f
Marco Christiani <mchristiani@sei.cmu.edu>
2023-12-08 22:04:27 -0500

    d4
    Signed-off-by: Marco Christiani <mchristiani@sei.cmu.edu>

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const alloc = gpa.allocator();
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+var alloc = gpa.allocator();
 
 pub fn day1() void {
     var data = @embedFile("day1.txt");
@@ -184,20 +184,173 @@ pub fn day2() !void {
     std.debug.print("part2: {d}\n", .{sum});
 }
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn day3() void {}
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+fn day4() !f64 {
+    const data = @embedFile("day4.txt");
+    // const data =
+    //     \\Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    //     \\Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    //     \\Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    //     \\Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    //     \\Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    //     \\Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    // ;
+    comptime var line_iter = std.mem.tokenizeScalar(u8, data, '\n');
+    var i: u32 = 0;
+    var total: f64 = 0;
+    while (line_iter.next()) |line| : (i += 1) {
+        var rest_iter = std.mem.tokenizeSequence(u8, line, ": ");
+        _ = rest_iter.next();
+        var hand_iter = std.mem.tokenizeScalar(u8, rest_iter.next().?, '|');
+        var winning_cards_iter = std.mem.tokenizeScalar(u8, hand_iter.next().?, ' ');
+        var my_cards_iter = std.mem.tokenizeScalar(u8, hand_iter.next().?, ' ');
+        // var count = count_intersection_iter(u8, &winning_cards_iter, &my_cards_iter);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+        var count: u32 = 0;
+        var set = std.AutoHashMap(usize, void).init(alloc);
+        defer set.deinit();
+        while (my_cards_iter.next()) |e| {
+            var num = try std.fmt.parseInt(usize, e, 10);
+            try set.put(num, {});
+        }
+        while (winning_cards_iter.next()) |e| {
+            var num = try std.fmt.parseInt(usize, e, 10);
+            if (set.contains(num)) {
+                count += 1;
+            }
+            try set.put(num, {});
+        }
+        const score: f32 = switch (count) {
+            0 => 0,
+            else => @exp2(@as(f32, @floatFromInt(count)) - 1),
+        };
+        std.debug.print("{d} -> {d}\n", .{ count, score });
+        total += score;
+    }
+    return total;
+}
 
-    try bw.flush(); // don't forget to flush!
+fn day4p2() !u32 {
+    const data = @embedFile("day4.txt");
+    // const data =
+    //     \\Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    //     \\Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    //     \\Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    //     \\Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    //     \\Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    //     \\Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    // ;
+    comptime var line_iter = std.mem.tokenizeScalar(u8, data, '\n');
+    var i: u32 = 0;
+    var total: u64 = 0;
+    var scores = std.ArrayList(u32).init(alloc);
+    defer scores.deinit();
+    while (line_iter.next()) |line| : (i += 1) {
+        var rest_iter = std.mem.tokenizeSequence(u8, line, ": ");
+        _ = rest_iter.next();
+        var hand_iter = std.mem.tokenizeScalar(u8, rest_iter.next().?, '|');
+        var winning_cards_iter = std.mem.tokenizeScalar(u8, hand_iter.next().?, ' ');
+        var my_cards_iter = std.mem.tokenizeScalar(u8, hand_iter.next().?, ' ');
+
+        var count: u32 = 0;
+        var set = std.AutoHashMap(usize, void).init(alloc);
+        defer set.deinit();
+        while (my_cards_iter.next()) |e| {
+            var num = try std.fmt.parseInt(usize, e, 10);
+            try set.put(num, {});
+        }
+        while (winning_cards_iter.next()) |e| {
+            var num = try std.fmt.parseInt(usize, e, 10);
+            if (set.contains(num)) {
+                count += 1;
+            }
+            try set.put(num, {});
+        }
+
+        try scores.append(count);
+        total += count;
+    }
+    std.debug.print("{d}\n", .{total});
+    // iterate a second time, add up number of cards
+    var score_slice = try scores.toOwnedSlice();
+    var temp = try std.BoundedArray(u32, 256).init(score_slice.len);
+    var card_counts = temp.slice();
+
+    for (card_counts, 0..) |_, k| {
+        card_counts[k] = 1;
+    }
+    var total_cards: u32 = @intCast(score_slice.len);
+    line_iter.reset();
+    i = 0;
+    std.debug.print("{d}\n", .{score_slice});
+    std.debug.print("total_cards={d}\n", .{total_cards});
+    while (line_iter.next()) |_| : (i += 1) {
+        var start_idx = i + 1;
+        var end_idx = i + score_slice[i] + 1;
+        var len: u32 = @intCast(end_idx);
+        if (end_idx > len) {
+            end_idx = len;
+        }
+        if (start_idx > len) {
+            start_idx = len;
+        }
+        for (start_idx..end_idx) |m| {
+            card_counts[m] += card_counts[i];
+        }
+
+        std.debug.print("score: {d}\n", .{score_slice[i]});
+
+        for (card_counts, 0..) |value, j| {
+            std.debug.print("\t{d}:{d}\n", .{ j, value });
+        }
+    }
+    total_cards = sum_slice(card_counts);
+    return total_cards;
+}
+
+fn sum_slice(slice: []u32) u32 {
+    var total: u32 = 0;
+    for (slice) |s| {
+        total += s;
+    }
+    return total;
+}
+
+fn iter_to_buffer(comptime T: type, buffer: [][]const T, iter: *std.mem.TokenIterator(T, .scalar)) void {
+    var i: usize = 0; // heap who?
+    while (iter.next()) |item| : (i += 1) {
+        if (i >= buffer.len) break; // Prevent buffer overflow
+        buffer[i] = item;
+    }
+}
+
+fn count_intersection_iter(comptime T: type, iter1: *std.mem.TokenIterator(T, .scalar), iter2: *std.mem.TokenIterator(T, .scalar)) std.mem.Allocator.Error!usize {
+    var result: usize = 0;
+    var set = std.AutoHashMap([]const T, void).init(alloc);
+    defer set.deinit();
+    while (iter1.next()) |e| try set.put(e, {});
+    while (iter2.next()) |e| {
+        if (set.contains(e)) {
+            result += 1;
+        }
+        try set.put(e, {});
+    }
+    return result;
+}
+
+fn count_intersection_arr(comptime T: type, comptime arr1: []const T, comptime arr2: []const T) std.mem.Allocator.Error!usize {
+    var result: usize = 0;
+    var set = std.AutoHashMap(T, void).init(alloc);
+    defer set.deinit();
+    for (arr1) |e| try set.put(e, {});
+    for (arr2) |e| {
+        if (set.contains(e)) {
+            result += 1;
+        }
+        try set.put(e, {});
+    }
+    return result;
 }
 
 test "day 1" {
@@ -207,4 +360,16 @@ test "day 1" {
 
 test "day 2" {
     try day2();
+}
+
+test "day 4" {
+    const part1: u32 = try day4();
+    const part2: u32 = try day4p2();
+    std.debug.print("part1: {d}\n", .{part1});
+    std.debug.print("part2: {d}\n", .{part2});
+}
+
+test "count intersection" {
+    const count = try count_intersection_arr(usize, &[_]usize{ 1, 2, 3 }, &[_]usize{ 1, 2, 3 });
+    std.debug.print("{d}\n", .{count});
 }
